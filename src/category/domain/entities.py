f245654e7701b213249a524f0f179ee5cc6594a1
zerefstbl <duduoli11@hotmail.com>
2023-08-21 20:43:29 -0300

    (feat): Added id in categoryh entity

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -3,14 +3,18 @@ from typing import Optional
 
 from dataclasses import dataclass, field
 
+import uuid
 
-@dataclass()
+@dataclass(kw_only=True)
 class Category:
   
-  name: str
-  description: Optional[str] = None
-  is_active: Optional[bool] = True
-  created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+    id: uuid.UUID = field(
+        default_factory=lambda: uuid.uuid4()
+    )
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(
+        default_factory=lambda: datetime.now()
+    )
   
-
-
